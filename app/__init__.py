5a490aca42e61ad1f72202c86dfc5e91d1df8182
jkkicks <jkkicks1@gmail.com>
2025-08-17 16:25:10 -0700

    Bump version to v1.0.0

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 """Kernel API - CAD to Mesh Conversion Service"""
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 __author__ = "Kernel API Team"
 __description__ = "RESTful API for converting CAD files (STEP, IGES, BREP) to mesh formats (STL, OBJ, GLTF)"
